6a9bde3e8ca72afdd9e00ecd9159a8fd53cc5299
rachelKuroneko <jingyi-tan@hotmail.com>
2017-03-02 17:06:15 +1000

    Initial checkin of simple code.

---
+++ SimpleCalculation/src/simpleCal/Test1.java
@@ -0,0 +1,15 @@
+package simpleCal;
+
+import javax.swing.*;
+import java.util.*;
+public class Test1 {
+	public static void main(String args[]){
+		int x, y, result;
+		x = 20;
+		y = 5;
+		result = x / y;
+		JOptionPane.showMessageDialog(null, result);
+	}
+
+	
+}
